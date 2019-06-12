ae356696d0a5d5b7c53b1a884b94a5f87373fa98
Bobby <bobbyhplau@gmail.com>
2019-06-12 21:01:24 +0000

    Add a new tweet to the database.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,54 +1,5 @@
 $(document).ready(function() {
 
-    //const tweetDB = JSON.parse('../server/data-files/initial-tweets.json');
-
-    const data = [{
-            "user": {
-                "name": "Newton",
-                "avatars": {
-                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-                },
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": {
-                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-                },
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        },
-        {
-            "user": {
-                "name": "Johann von Goethe",
-                "avatars": {
-                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-                },
-                "handle": "@johann49"
-            },
-            "content": {
-                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-            },
-            "created_at": 1461113796368
-        }
-    ];
-
     const parseDate = function(timeCode) {
         let msPerMinute = 60 * 1000;
         let msPerHour = 60 * msPerMinute;
@@ -82,6 +33,18 @@ $(document).ready(function() {
         return 'aproximately ' + Math.round(elapsed / msPerYear) + ' years ago';
     }
 
+    const loadTweets = function() {
+        $.ajax({
+            method: 'GET',
+            url: '/tweets',
+            dataType: 'json'
+        }).then(function(result) {
+            return renderTweets(result);
+        });
+    }
+
+    loadTweets();
+
     const createTweetElement = function(tweetObj) {
         let realname = tweetObj.user.name;
         let username = tweetObj.user.handle;
@@ -113,9 +76,7 @@ $(document).ready(function() {
 
     function renderTweets(tweets) {
         for (let tweet of tweets) {
-            $('#tweets-container').append(createTweetElement(tweet));
+            $('#tweets-container').prepend(createTweetElement(tweet));
         }
     }
-
-    renderTweets(data);
 });
